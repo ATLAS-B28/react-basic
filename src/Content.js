f441b8eef5b7fa962716831ecc7d2e7333fa7525
aditya <adityacbhambere28@gmail.com>
2023-02-06 21:26:12 +0530

    handle events in react sample

--- src/Content.js
+++ src/Content.js
@@ -1,23 +1,31 @@
 import React from "react";
 const Content = () => {
+  //now we will look at handling events in react
+  const handleNameChange = () => {
+    const names = ['Aditya','Kakashi','Aranyaka']
+    const int = Math.floor(Math.random()*3)
+    return names[int]
+  }
+  const handleClick = () => {
+    console.log('clicked it')
+  }
+  const handleClickTwo = (name) => {
+    console.log(`${name} clicked it`)
+  }
+  const handleClickThree = (e) => {
+    console.log(e.target.innerText)
+  }
+  
   return (
     <main className="App-content">
-      <ul>
-        <li>React JSX</li>
-        <li>Functional Components</li>
-        <li>CSS in JSX</li>
-        <li>Props</li>
-        <li>State</li>
-        <li>Hooks</li>
-        <li>Custom Hooks</li>
-        <li>In-built Hooks</li>
-        <li>React Forms</li>
-        <li>React Auth and JWT</li>
-        <li>State Management with Redux</li>
-        <li>Auth with Redux</li>
-        <li>Aysnc Redux</li>
-        <li>RTK query</li>
-      </ul>
+      <p onDoubleClick={handleClick}>
+        Hello {handleNameChange()}
+      </p>
+      <button onClick={handleClick}>Click it</button>
+      <button onClick={()=>handleClickTwo('Aditya')}>Click it</button>
+      {/*here we use an anonymus function to pass parameter and
+       trigger the function */}
+      <button onClick={(e)=>handleClickThree(e)}>Click to acces event object</button>
     </main>
   );
 };
